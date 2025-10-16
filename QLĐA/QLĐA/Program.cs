cd74a8341abb0e6860a45c80f1efe9d1a815fdb8
Bien <superkhazix0@gmail.com>
2025-10-16 18:31:34 +0700

    sửa program cs

--- QLĐA/QLĐA/Program.cs
+++ QLĐA/QLĐA/Program.cs
@@ -1,9 +1,10 @@
-﻿using DevExpress.DXperience.Demos;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Project_QLDA;
 
 namespace QLĐA
 {
