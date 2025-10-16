536f004fc6c4f3a5b33a784d5c39415bb68bdf36
Bien <superkhazix0@gmail.com>
2025-10-16 18:14:10 +0700

    Form đầu tiên

---
+++ QLĐA/QLĐA/frmMain.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Project_QLDA
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.ribbon = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.btnExit = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem3 = new DevExpress.XtraBars.BarButtonItem();
+            this.btnTraCuuGiangVien = new DevExpress.XtraBars.BarButtonItem();
+            this.btnTraCuuDoAn = new DevExpress.XtraBars.BarButtonItem();
+            this.btnTraCuuSinhVien = new DevExpress.XtraBars.BarButtonItem();
+            this.btnLogIn = new DevExpress.XtraBars.BarButtonItem();
+            this.barUser = new DevExpress.XtraBars.BarStaticItem();
+            this.barTimer = new DevExpress.XtraBars.BarStaticItem();
+            this.barClock = new DevExpress.XtraBars.BarStaticItem();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPage2 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbon
+            // 
+            this.ribbon.ExpandCollapseItem.Id = 0;
+            this.ribbon.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbon.ExpandCollapseItem,
+            this.barButtonItem1,
+            this.btnExit,
+            this.barButtonItem3,
+            this.btnTraCuuGiangVien,
+            this.btnTraCuuDoAn,
+            this.btnTraCuuSinhVien,
+            this.btnLogIn,
+            this.barUser,
+            this.barTimer,
+            this.barClock});
+            this.ribbon.Location = new System.Drawing.Point(0, 0);
+            this.ribbon.MaxItemId = 17;
+            this.ribbon.Name = "ribbon";
+            this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1,
+            this.ribbonPage2});
+            this.ribbon.Size = new System.Drawing.Size(1699, 260);
+            this.ribbon.StatusBar = this.ribbonStatusBar;
+            this.ribbon.Click += new System.EventHandler(this.ribbon_Click);
+            // 
+            // barButtonItem1
+            // 
+            this.barButtonItem1.Id = 3;
+            this.barButtonItem1.Name = "barButtonItem1";
+            this.barButtonItem1.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem1_ItemClick_1);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Caption = "Kết thúc";
+            this.btnExit.Id = 4;
+            this.btnExit.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnExit.ImageOptions.SvgImage")));
+            this.btnExit.Name = "btnExit";
+            this.btnExit.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnExit_ItemClick);
+            // 
+            // barButtonItem3
+            // 
+            this.barButtonItem3.Caption = "barButtonItem3";
+            this.barButtonItem3.Id = 5;
+            this.barButtonItem3.Name = "barButtonItem3";
+            // 
+            // btnTraCuuGiangVien
+            // 
+            this.btnTraCuuGiangVien.Caption = "Tra cứu giảng viên";
+            this.btnTraCuuGiangVien.Id = 10;
+            this.btnTraCuuGiangVien.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnTraCuuGiangVien.ImageOptions.Image")));
+            this.btnTraCuuGiangVien.Name = "btnTraCuuGiangVien";
+            this.btnTraCuuGiangVien.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.btnTraCuuGiangVien.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnTraCuuGiangVien_ItemClick);
+            // 
+            // btnTraCuuDoAn
+            // 
+            this.btnTraCuuDoAn.Caption = "Tra cứu đồ án";
+            this.btnTraCuuDoAn.Id = 11;
+            this.btnTraCuuDoAn.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnTraCuuDoAn.ImageOptions.Image")));
+            this.btnTraCuuDoAn.Name = "btnTraCuuDoAn";
+            this.btnTraCuuDoAn.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.btnTraCuuDoAn.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnTraCuuDoAn_ItemClick);
+            // 
+            // btnTraCuuSinhVien
+            // 
+            this.btnTraCuuSinhVien.Caption = "Tra cứu sinh viên";
+            this.btnTraCuuSinhVien.Id = 12;
+            this.btnTraCuuSinhVien.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnTraCuuSinhVien.ImageOptions.Image")));
+            this.btnTraCuuSinhVien.Name = "btnTraCuuSinhVien";
+            this.btnTraCuuSinhVien.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.btnTraCuuSinhVien.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem2_ItemClick_1);
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Caption = "Đăng nhập";
+            this.btnLogIn.Id = 13;
+            this.btnLogIn.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnLogIn.ImageOptions.Image")));
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.btnLogIn.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnDangNhap_ItemClick);
+            // 
+            // barUser
+            // 
+            this.barUser.Caption = "Người dùng: Khách";
+            this.barUser.Id = 14;
+            this.barUser.Name = "barUser";
+            // 
+            // barTimer
+            // 
+            this.barTimer.Id = 15;
+            this.barTimer.Name = "barTimer";
+            this.barTimer.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barTimer_ItemClick);
+            // 
+            // barClock
+            // 
+            this.barClock.Caption = "barStaticItem1";
+            this.barClock.Id = 16;
+            this.barClock.Name = "barClock";
+            this.barClock.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barClock_ItemClick);
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1});
+            this.ribbonPage1.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("ribbonPage1.ImageOptions.SvgImage")));
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "Hệ thống";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnLogIn);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnExit);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barButtonItem1);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "Phiên làm việc";
+            // 
+            // ribbonPage2
+            // 
+            this.ribbonPage2.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup2});
+            this.ribbonPage2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("ribbonPage2.ImageOptions.Image")));
+            this.ribbonPage2.Name = "ribbonPage2";
+            this.ribbonPage2.Text = "Tra cứu";
+            // 
+            // ribbonPageGroup2
+            // 
+            this.ribbonPageGroup2.ItemLinks.Add(this.btnTraCuuDoAn);
+            this.ribbonPageGroup2.ItemLinks.Add(this.btnTraCuuGiangVien);
+            this.ribbonPageGroup2.ItemLinks.Add(this.btnTraCuuSinhVien);
+            this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+            this.ribbonPageGroup2.Text = "Danh mục tra cứu";
+            // 
+            // ribbonStatusBar
+            // 
+            this.ribbonStatusBar.ItemLinks.Add(this.barUser);
+            this.ribbonStatusBar.ItemLinks.Add(this.barTimer);
+            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 948);
+            this.ribbonStatusBar.Name = "ribbonStatusBar";
+            this.ribbonStatusBar.Ribbon = this.ribbon;
+            this.ribbonStatusBar.Size = new System.Drawing.Size(1699, 36);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(647, 266);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(536, 548);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14F);
+            this.label1.Location = new System.Drawing.Point(759, 817);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Phần mềm quản lý đồ án";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14F);
+            this.label2.Location = new System.Drawing.Point(711, 849);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(418, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Khoa Hệ thống thông tin quản lý";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(455, 661);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(0, 19);
+            this.labelControl1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(641, 881);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(585, 33);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Vui lòng đăng nhập để sử dụng hệ thống...";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(1699, 984);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ribbonStatusBar);
+            this.Controls.Add(this.ribbon);
+            this.Name = "frmMain";
+            this.Ribbon = this.ribbon;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StatusBar = this.ribbonStatusBar;
+            this.Text = "Phần mềm quản lý đồ án";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbon;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+        private DevExpress.XtraBars.BarButtonItem btnExit;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem3;
+        private DevExpress.XtraBars.BarButtonItem btnProfile;
+        private DevExpress.XtraBars.BarButtonItem btnSave;
+        private DevExpress.XtraBars.BarButtonItem btnSettings;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage2;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+        private DevExpress.XtraBars.BarButtonItem btnTraCuuGiangVien;
+        private DevExpress.XtraBars.BarButtonItem btnTraCuuDoAn;
+        private DevExpress.XtraBars.BarButtonItem btnTraCuuSinhVien;
+        private DevExpress.XtraBars.BarButtonItem btnLogIn;
+        private DevExpress.XtraBars.BarStaticItem barUser;
+        private DevExpress.XtraBars.BarStaticItem barTimer;
+        private DevExpress.XtraBars.BarStaticItem barClock;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ QLĐA/QLĐA/frmMain.cs
@@ -0,0 +1,112 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_QLDA
+{
+    public partial class frmMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+         
+
+        }
+
+        private void barButtonItem1_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem1_ItemClick_1(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem4_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem2_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void ribbon_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void btnTraCuuGiangVien_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void btnTraCuuDoAn_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem2_ItemClick_1(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void btnDangNhap_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barClock_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            barTimer.Caption = System.DateTime.Now.ToString();
+        }
+
+        private void barTimer_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
