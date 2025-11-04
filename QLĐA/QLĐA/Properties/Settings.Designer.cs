52f6a61779bca57dd20ec89c9ed8b56331d36ed6
Bien <superkhazix0@gmail.com>
2025-11-04 16:01:11 +0700

    Đổi tên email = tên đăng nhập

--- QLĐA/QLĐA/Properties/Settings.Designer.cs
+++ QLĐA/QLĐA/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace QLĐA.Properties
-{
-
-
+namespace QLĐA.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-OREV608\\SQLEXPRESS;Initial Catalog=QuanLyDoAnTotNghiep;Integr" +
+            "ated Security=True;TrustServerCertificate=True")]
+        public string QuanLyDoAnTotNghiepConnectionString {
+            get {
+                return ((string)(this["QuanLyDoAnTotNghiepConnectionString"]));
             }
         }
     }
