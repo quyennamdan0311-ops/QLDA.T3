fa747ab5ca6f38a9e69bb97537cbedebfe779306
Le Duc Quyen <quyen.namdan0311@gmail.com>
2025-11-12 22:56:31 +0700

    Màn hình chính

---
+++ QLĐA/QLĐA/FluentDesignForm1.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLĐA
+{
+    public partial class FluentDesignForm1 : DevExpress.XtraBars.FluentDesignSystem.FluentDesignForm
+    {
+        public FluentDesignForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void fluentDesignFormContainer1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
